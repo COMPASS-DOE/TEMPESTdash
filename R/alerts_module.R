3c5b5d69cd793e08e8dbc4a62233398e72a3e17a
Ben Bond-Lamberty <bondlamberty@pnnl.gov>
2023-06-23 07:01:07 -0400

    Move alert-sending code to alerts module file
    Former-commit-id: 1c7575f18c94f7cf15c37f364aa8aa35a86248f0

--- R/alerts_module.R
+++ R/alerts_module.R
@@ -43,3 +43,47 @@ alertsServer <- function(id) {
 
         })
 }
+
+# Called by the server when the alertInvalidate timer fires
+# Parameter dd is the dropbox_data() reactive from the server
+send_alerts <- function(dd) {
+    msg <- paste(
+        paste0("System status as of: ",
+               with_tz(Sys.time(), tzone = "America/New_York"), " EDT"),
+        "",
+        paste0("Sapflow: ", dd()$sapflow_bdg$percent_in),
+        paste0("TEROS: ", dd()$teros_bdg$percent_in),
+        paste0("Aquatroll: ", dd()$aquatroll_bdg$percent_in),
+        paste0("Battery: ", dd()$battery_bdg$percent_in),
+        sep = "\n")
+
+    for(i in seq_len(nrow(TEXT_MSG_USERS))) {
+        phone_number <- TEXT_MSG_USERS$number[i]
+        carrier <- TEXT_MSG_USERS$carrier[i]
+
+        carrier_email <- if(carrier == "Verizon") {
+            carrier_email <- "@vtext.com"
+        } else if(carrier == "AT&T") {
+            carrier_email <- "@txt.att.net"
+        } else if(carrier == "Sprint") {
+            carrier_email <- "@messaging.sprintpcs.com"
+        } else if(carrier == "T-Mobile") {
+            carrier_email <- "@tmomail.net"
+        }
+
+        email <- paste0(phone_number, carrier_email)
+
+        # Wrap this in a try so that if not authorized the app doesn't stop
+        try({
+            text_msg <- gm_mime() %>%
+                gm_to(email) %>%
+                gm_from("compassfme.tools@gmail.com") %>%
+                gm_text_body(msg) # CHANGE THIS
+
+            # need to add how often to send, right now only once
+
+            gm_send_message(text_msg)
+        })
+    }
+
+}
