d920a1f9a87c705218db69cde8c036f5d37b924e
Ben Bond-Lamberty <bondlamberty@pnnl.gov>
2023-06-22 21:20:53 -0400

    Load app environment but don't use Dropbox if testing

--- tests/testthat/setup-shinytest2.R
+++ tests/testthat/setup-shinytest2.R
@@ -1,2 +1,6 @@
+# GitHub Actions will set this, but it's handy locally as well,
+# as the dashboard code will use test data and not go to Dropbox
+Sys.setenv(CI = "true")
+
 # Load application support files into testing environment
-#shinytest2::load_app_env()
+shinytest2::load_app_env()
