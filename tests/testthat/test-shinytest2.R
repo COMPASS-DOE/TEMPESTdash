dcf48fdd2fc54da273a487ebd46ecad0c4202c42
Ben Bond-Lamberty <bondlamberty@pnnl.gov>
2023-06-27 11:57:05 -0400

    Update gear's event_start default based on data

--- tests/testthat/test-shinytest2.R
+++ tests/testthat/test-shinytest2.R
@@ -2,6 +2,8 @@ library(shinytest2)
 
 
 test_that("{shinytest2} recording: TEMPESTdash", {
+  # Some UI elements (in particular, dropdownButton) seem to have some
+  # randomness. Need to set a seed here so that everything is reproducible
   app <- AppDriver$new(name = "TEMPESTdash", seed = 1234, height = 739, width = 1087)
   app$expect_values()
 })
