1fc34f52728bcb03c96c8cbfe9045eb4fe1f0492
Stephanie Pennington <stephanie.pennington@pnnl.gov>
2026-07-07 14:49:16 -0400

    Remove process_dir copies

--- R/process_teros.R
+++ R/process_teros.R
@@ -44,7 +44,7 @@ process_teros <- function(token, datadir) {
                Logger = as.character(Logger)) %>%
         select(Timestamp, Plot, ID, Grid_Square, Logger, variable, Depth, value) -> t
 
-    process_dir(datadir = datadir, pattern = "84_Teros21", read_datalogger_file, dropbox_token = token) %>%
+    compasstools:::process_dir(datadir = datadir, pattern = "84_Teros21", read_datalogger_file, dropbox_token = token) %>%
         pivot_longer(starts_with("Teros"), names_to = "channel") %>%
         filter(!is.na(value)) %>%
         rename(Timestamp = TIMESTAMP) %>%
@@ -70,7 +70,7 @@ process_teros <- function(token, datadir) {
         select(Timestamp, Plot, ID, Grid_Square, Logger, variable, Depth, value) -> ert_21
 
 
-    process_dir(datadir = datadir, pattern = "84_Teros12", read_datalogger_file, dropbox_token = token) %>%
+    compasstools:::process_dir(datadir = datadir, pattern = "84_Teros12", read_datalogger_file, dropbox_token = token) %>%
         pivot_longer(starts_with("Teros"), names_to = "channel") %>%
         filter(!is.na(value)) %>%
         rename(Timestamp = TIMESTAMP) %>%
